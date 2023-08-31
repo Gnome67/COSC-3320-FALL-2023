0f1fb19a64d65956ef33e1e74fa8b96c01fda346
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-08-30 20:22:13 -0500

    Update main.py

--- Find maximum difference/main.py
+++ Find maximum difference/main.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        
+        for price in prices[1:]:
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+            
+        return max_profit
