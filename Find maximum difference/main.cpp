8a94e4bedbc18899d6c9528d83611c1edc7e407c
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-08-30 20:20:46 -0500

    Update main.cpp

--- Find maximum difference/main.cpp
+++ Find maximum difference/main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int maxDifference(vector<int> question)
+{
+    int minimum = question[0];
+		int maximum = 0;
+    for(int i=1;i<question.size();i++)
+    {
+      maximum = max(maximum,question[i]-minimum);
+      minimum = min(question[i],minimum);
+    }
+    return maximum;
+}
+
+int main()
+{
+  int n;
+  int value;
+  cin >> n;
+  vector<int> question;
+  for(int x = 0; x < n; x++)
+  {
+    cin >> value;
+    question.push_back(value);
+  }
+	int answer = maxDifference(question);
+	cout << answer;
+	return 0;
+}
