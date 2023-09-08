555fcd593aa0e6cf06934ebb6e2e57c3869821f5
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-09-08 16:28:45 -0500

    Update main.cpp

--- Last Remaining Number/main.cpp
+++ Last Remaining Number/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+using namespace std;
+
+int lastRemaining(int n)
+{
+    if(n==1) { return 1; }
+    return 2*((n/2)-lastRemaining(n/2)+1);
+}
+
+int main()
+{
+   int n = 0;
+   cin >> n;
+   int a = lastRemaining(n);
+   cout << a;
+}
