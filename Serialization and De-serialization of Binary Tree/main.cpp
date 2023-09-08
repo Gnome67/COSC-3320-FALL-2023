1d3b0cf2775f5f0210371d769457ab7773036f3b
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-09-08 17:23:43 -0500

    Update main.cpp

--- Serialization and De-serialization of Binary Tree/main.cpp
+++ Serialization and De-serialization of Binary Tree/main.cpp
@@ -1,3 +1,9 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -16,19 +22,15 @@ public:
         istringstream in(data);
         return decode(in);
     }
-    TreeNode* buildTree(std::string data) {
-        std::istringstream ss(data);
-        return buildTreeHelper(ss);
-    }
     
 private:
     void encode(TreeNode* root, ostringstream& out) {
         if (root == NULL)
         {
-            out << "N ";
+            cout << "N ";
             return;
         }
-        out << root->val << " ";
+        cout << root->val << " ";
         encode(root->left, out);
         encode(root->right, out);
     }
@@ -42,21 +44,12 @@ private:
         return root;
     }
 };
-    TreeNode* buildTreeHelper(std::istringstream& ss) {
-        std::string token;
-        ss >> token;
-        if (token == "{}") { return nullptr; }
-        TreeNode* root = new TreeNode(std::stoi(token));
-        root->left = buildTreeHelper(ss);
-        root->right = buildTreeHelper(ss);
-        return root;
-    }
 
 int main()
 {
     string inputN = "";
     getline(cin, inputN);
-    Codec codec, ser, deser;
-    TreeNode* root = codec.buildTree(inputN);
-    TreeNode* ans = deser.deserialize(ser.serialize(root));
+    Codec ser, deser;
+    // initialize root
+    TreeNode* ans = deser.deserialize(ser.serialize(ans));
 }
