b1b11ccf576418bb229db4ee39b50026aab252df
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-10-02 18:35:40 -0500

    Update main.cpp

--- Recursive Method to Traverse Staircase/main.cpp
+++ Recursive Method to Traverse Staircase/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int countWaysToClimbStairs(int height, int maxSteps)
+{
+    vector<int> dp(height + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= height; i++) { for (int j = 1; j <= maxSteps && i - j >= 0; j++) { dp[i] += dp[i - j]; } }
+    return dp[height];
+}
+
+int main()
+{
+    int height, maxSteps;
+    cin >> height >> maxSteps;
+    int ways = countWaysToClimbStairs(height, maxSteps);
+    cout << ways;
+    return 0;
+}
