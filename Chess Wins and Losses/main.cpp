1e17d29e1c3be6e0a28e73f37bf4ddda9a036cb6
Gnome67 <102388813+Gnome67@users.noreply.github.com>
2023-11-13 01:07:51 -0600

    Update main.cpp

--- Chess Wins and Losses/main.cpp
+++ Chess Wins and Losses/main.cpp
@@ -1,1 +1,50 @@
+// fails test case 7, fix later
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+bool isInSlump(const string& games) {
+    int winCount = 0, lossCount = 0, drawCount = 0;
+    int gamesSinceWin = 0;
+    istringstream ss(games);
+    char result;
+
+    while (ss >> result) {
+        // Update counts based on the result of each game
+        if (result == 'W') {
+            winCount++;
+            gamesSinceWin = 0;
+        } else if (result == 'L') {
+            lossCount++;
+            gamesSinceWin++;
+        } else if (result == 'D') {
+            drawCount++;
+            gamesSinceWin++;
+        }
+
+        // Check for slump conditions
+        if (lossCount > winCount + drawCount) return true;
+        if (gamesSinceWin >= 4) return true;
+
+        // Keep only the last three games' results
+        if (winCount + lossCount + drawCount > 3) {
+            if (result == 'W') drawCount--;
+            else if (result == 'L') winCount--;
+            else if (result == 'D') lossCount--;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    string games;
+    getline(cin, games);
+
+    cout << (isInSlump(games) ? "T" : "F") << endl;
+
+    return 0;
+}
